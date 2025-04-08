630903c1f83db344fd20bce91897e943b5807275
Kenny Carvajal <kennycarvajal24@gmail.com>
2025-04-08 19:47:08 -0400

    Set up Sanity CMS client and initial setup

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,25 +1,10 @@
-import {apiVersion, dataset, projectId, studioUrl} from '@/sanity/lib/api'
-import {createClient} from 'next-sanity'
+import { createClient } from 'next-sanity'
+
+import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
-  perspective: 'published',
-  stega: {
-    studioUrl,
-    logger: console,
-    filter: (props) => {
-      if (props.sourcePath.at(-1) === 'title') {
-        return true
-      }
-
-      return props.filterDefault(props)
-    },
-  },
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
-
-console.warn(
-  'This template is using stega to embed Content Source Maps, see more information here: https://www.sanity.io/docs/loaders-and-overlays#26cf681fadd4',
-)
